888415d92382d1024fd37c7e519b8b894915694e
Reda Hmeid <reda@hmeid.com>
2022-08-08 10:00:36 +0100

    I split it but forgot to add the new file
    Signed-off-by: Reda Hmeid <reda@hmeid.com>

---
+++ compare.py
@@ -0,0 +1,113 @@
+from pymongo import MongoClient
+# pprint library is used to make the output look more pretty
+from pprint import pprint
+import os
+import time
+import datetime
+
+from dotenv import load_dotenv
+load_dotenv()
+from datetime import date
+# connect to MongoDB, change the << MONGODB URL >> to reflect your own connection string
+client = MongoClient(os.environ["MONGO_URL"])
+db=client[os.environ["DB"]]
+previous_db=client[os.environ["PREVIOUS_SEASON"]]
+
+GW = int(os.environ["GW"])
+MODEL_VERSION="0.0.1"
+HOME_XG_FIELD = "Home xG"
+HOME_G_FIELD = "Home goals"
+AWAY_G_FIELD = "Away goals"
+HOME_FIELD = "Home"
+AWAY_XG_FIELD = "Away xG"
+AWAY_FIELD = "Away"
+HOME_POINTS_FIELD="Home points"
+AWAY_POINTS_FIELD="Away points"
+HOME_POSITION_FIELD="Home position"
+AWAY_POSITION_FIELD="Away position"
+ACTUAL = "Actual"
+PREDICTED = "Predicted"
+
+
+
+def trueActualResult(homeOrAway):
+
+  id = "$" + homeOrAway
+  points = "$Points " + homeOrAway
+  pointsSearch = [
+    {
+      '$group': {
+        '_id': id,
+        'points': {
+          '$sum': points
+        }
+      }
+    },
+        {
+            '$sort': {
+                'points': -1
+            }
+        }
+  ]
+  matches = db.matches.aggregate(pointsSearch)
+
+  for match in matches:
+      db.actualResult.update_one({"team":match["_id"]},{"$inc":{"points":match["points"]}},True)
+      
+
+def runResults():
+  trueActualResult(HOME_FIELD)
+  trueActualResult(AWAY_FIELD)
+  
+  pointsSearch = [
+   
+        {
+            '$sort': {
+                'points': -1
+            }
+        }
+  ]
+  results = db.actualResult.aggregate(pointsSearch)
+  i=1
+  for result in results:
+      db.actualResult.update_one({"team":result["team"]},{"$set":{"position":i}},True)
+      i=i+1
+
+def comparison(gw=GW,model_version=MODEL_VERSION):
+  predictions = db.prediction.find({"predicted_GW":gw,"model_version":model_version}).sort("position",1)
+  correct = 0
+  within_one = 0
+
+  points_correct=0
+  points_within_5=0
+  points_within_10=0
+  
+  print("|**Team**|**Predicted Position**|**Actual Position**|**Difference**|**Predicted Points**|**Actual Points**|**Difference**|")
+  print("|-------------------|------------|------------|--------------|--------------|----------|-----------|")
+    
+  for prediction in predictions:
+    # print("What was the prediction %s ",prediction)
+    team = db.actualResult.find_one({"team":prediction["team"]})
+    # print("|"+prediction["team"]+"|"+str(prediction["position"])+"|"+str(team["position"])+"|"+str(prediction["position"]-team["position"])+"|"+str(prediction["points"])+"|"+str(team["points"])+"|"+str(prediction["points"]-team["points"])+"|")
+    # print("|-------------------|------------|------------|--------------|--------------|----------|-----------|")
+    correct = correct+1 if team["position"]==prediction["position"] else correct
+    within_one = within_one+1 if (team["position"]-prediction["position"]==1) else within_one
+    within_one = within_one+1 if (team["position"]-prediction["position"]==-1) else within_one
+  
+    points_correct = points_correct+1 if team["points"]==prediction["points"] else points_correct
+    points_within_5 = points_within_5+1 if (team["points"]-prediction["points"]<5 & team["points"]-prediction["points"]>-5) else points_within_5
+    points_within_10 = points_within_10+1 if (team["points"]-prediction["points"]<10 & team["points"]-prediction["points"]>-10) else points_within_10
+
+  pprint("How many correct "+str(correct))
+  pprint("How many within one "+str(within_one))
+
+  pprint("How many points predicted correctly "+str(points_correct))
+  pprint("How many predicted within 5 points "+str(points_within_5))
+  pprint("How many predicted within 10 points "+str(points_within_10))
+
+
+runResults()
+
+comparison()
+
+
